d5947b84b4ad39d9c0c0e7160c573f5f6d7b3328
abbsay <35399996+abbsay@users.noreply.github.com>
2024-12-30 07:17:53 +0800

    v0.0.13: 恢复全唐诗分类配置

--- sidebars.js
+++ sidebars.js
@@ -18,6 +18,75 @@ const sidebars = {
         'poetry/tang/poets/wangwei'
       ],
     },
+    {
+      type: 'category',
+      label: '全唐诗',
+      items: [
+        {
+          type: 'category',
+          label: '五言绝句',
+          items: [
+            'poetry/tang/quantangshi/libai/jingye',
+            'poetry/tang/quantangshi/libai/duzuojingting',
+            'poetry/tang/quantangshi/wangwei/luchai',
+            'poetry/tang/quantangshi/wangwei/songbie',
+            'poetry/tang/quantangshi/dufu/yueye'
+          ]
+        },
+        {
+          type: 'category',
+          label: '五言律诗',
+          items: [
+            'poetry/tang/quantangshi/dufu/chunwang',
+            'poetry/tang/quantangshi/dufu/kuangfu'
+          ]
+        },
+        {
+          type: 'category',
+          label: '七言绝句',
+          items: [
+            'poetry/tang/quantangshi/libai/wanglushan',
+            'poetry/tang/quantangshi/libai/zaofabaidicheng',
+            'poetry/tang/quantangshi/libai/qingpingdiao',
+            'poetry/tang/quantangshi/libai/shangyangtai',
+            'poetry/tang/quantangshi/libai/wangtianmen',
+            'poetry/tang/quantangshi/dufu/jiangnanfeng'
+          ]
+        },
+        {
+          type: 'category',
+          label: '七言律诗',
+          items: [
+            'poetry/tang/quantangshi/dufu/denggao',
+            'poetry/tang/quantangshi/dufu/qiuxing'
+          ]
+        },
+        {
+          type: 'category',
+          label: '乐府诗',
+          items: [
+            'poetry/tang/quantangshi/libai/jiangjinjiu',
+            'poetry/tang/quantangshi/libai/shudaonan'
+          ]
+        },
+        {
+          type: 'category',
+          label: '五言古诗',
+          items: [
+            'poetry/tang/quantangshi/libai/yuexiadu',
+            'poetry/tang/quantangshi/libai/bajiuwenyue',
+            'poetry/tang/quantangshi/dufu/wanghua'
+          ]
+        },
+        {
+          type: 'category',
+          label: '七言古诗',
+          items: [
+            'poetry/tang/quantangshi/libai/mengyoutianmu'
+          ]
+        }
+      ]
+    }
   ],
 
   // 宋词
